aeea27cd15d8b803378c5b9f2483ae26d09505c3
mlongano <mauro.longano@marconirovereto.it>
2021-03-21 12:09:57 +0100

    Remove purgeCss  in tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,5 @@
 module.exports = {
-  purge: {
-    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
-    enabled: process.env.NODE_ENV === 'production',
-    content: [
-      'components/**/*.vue',
-      'layouts/**/*.vue',
-      'pages/**/*.vue',
-      'templates/**/*.vue',
-      'plugins/**/*.js',
-      'gridsome.config.js'
-    ]
-  },
+  purge: false,
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {},
